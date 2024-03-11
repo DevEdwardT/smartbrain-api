6b5fc33967e226e3f1c1d49beef03dcdfef1e991
Edward Telecan <Devedwardt@gmail.com>
2024-03-11 17:33:55 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,119 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const bcrypt = require("bcrypt-nodejs");
+const cors = require("cors");
+const knex = require("knex");
+
+const db = knex({
+  client: "pg",
+  connection: {
+    host: "dpg-cnli3to21fec73c5qg30-a",
+    port: 5432,
+    user: "smartbrainsql_user",
+    password: process.env.DB_PASSWORD,
+    database: "smartbrainsql",
+    ssl: true,
+  },
+});
+
+db.select("*")
+  .from("users")
+  .then((data) => {});
+
+app.use(express.json());
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("success");
+});
+
+app.post("/signin", (req, res) => {
+  db.select("email", "hash")
+    .from("login")
+    .where("email", "=", req.body.email)
+    .then((data) => {
+      const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+      if (isValid && req.body.email.length > 1) {
+        return db
+          .select("*")
+          .from("users")
+          .where("email", "=", req.body.email)
+          .then((user) => {
+            res.json(user[0]);
+          })
+          .catch((err) => res.status(400).json("unable to get user"));
+      } else {
+        res.status(400).json("wrong credentials");
+      }
+    })
+    .catch((err) => res.status(400).json("wrong credentials"));
+});
+
+app.post("/register", (req, res) => {
+  const { email, name, password } = req.body;
+  const hash = bcrypt.hashSync(password);
+  if (
+    req.body.email.length > 1 &&
+    req.body.name.length > 1 &&
+    req.body.password.length > 1
+  ) {
+    db.transaction((trx) => {
+      trx
+        .insert({
+          hash: hash,
+          email: email,
+        })
+        .into("login")
+        .returning("email")
+        .then((loginEmail) => {
+          return trx("users")
+            .returning("*")
+            .insert({
+              email: loginEmail[0].email,
+              name: name,
+              joined: new Date(),
+            })
+            .then((user) => {
+              res.json(user[0]);
+            });
+        })
+        .then(trx.commit)
+        .catch(trx.rollback);
+    }).catch((err) => res.status(400).json("Unable to register!"));
+  } else {
+    console.log("fill in forms");
+  }
+});
+
+app.get("/profile/:id", (req, res) => {
+  const { id } = req.params;
+  db.select("*")
+    .from("users")
+    .where({ id })
+    .then((user) => {
+      if (user.length) {
+        res.json(user[0]);
+      } else {
+        res.status(400).json("not found");
+      }
+    })
+    .catch((err) => res.status(400).json("error getting user"));
+});
+
+app.put("/image", (req, res) => {
+  const { id } = req.body;
+  db("users")
+    .where("id", "=", id)
+    .increment("entries", 1)
+    .returning("entries")
+    .then((entries) => {
+      res.json(entries[0].entries);
+    })
+    .catch((err) => res.status(400).json("uanble to get entries"));
+});
+
+app.listen(3000, () => {
+  console.log("app is running on port 3000");
+});
